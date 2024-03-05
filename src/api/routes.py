8d0820b9c9e5f48dfe504c3b803bc1e4fcd3961a
Gustavo Reyes <yogireyes2@gmail.com>
2024-03-05 14:25:14 +0600

    Update api routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,13 +1,70 @@
 from flask import Blueprint, request, jsonify
 from src.helpers import token_required
-from src.models import db, CarCollection, car_schema, cars_schema
+from src.models import db, CarCollection, car_schema, cars_schema,User
 from flask_cors import cross_origin
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
+
 @api.route('/health-check')
 def health_check_view():
     return {'success': 'API is live'}
+
+
+@api.route('/signup', methods=['POST'])
+def api_signup():
+    data = request.json
+    first_name = data.get('first_name',None)
+    last_name = data.get('last_name',None)
+    email = data.get('email',None)
+    password = data.get('password',None)
+    
+    if not first_name or not last_name or not email or not password:
+        return jsonify({"error":"email, first_name, last_name,password is required"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error":"Email already registered.Choose a another email address"}), 400
+    
+    user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    response = {
+        'message': 'User successfully created.',
+        'email': email
+    }
+
+    return jsonify(response), 201
+
+
+
+@api.route('/login', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def api_login():
+    data = request.json
+
+    email = data.get('email',None)
+    password = data.get('password',None)
+    
+    if not email or not password:
+        return jsonify({"error":"email,password is required"}), 400
+
+    user = User.query.filter(User.email == email).first()
+
+    if user and user.check_password(password):
+        token = user.token
+        response = {
+            'message': 'Successfully logged in.',
+            'token': token
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Invalid credentials. Please try again.'
+        }
+        return jsonify(response), 401
+
 
 @api.route('/cars', methods=['GET'])
 @token_required
